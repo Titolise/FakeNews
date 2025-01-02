b290bc6f92fa29817ca3a3954ab8fb205171ad04
Titolise <gianfix01@gmail.com>
2025-01-02 12:24:02 +0100

    Update

--- DetectingFakeNews.py
+++ DetectingFakeNews.py
@@ -1,15 +1,12 @@
 #import the library
-
-import numpy as np 
 import pandas as pd
-import itertools
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import PassiveAggressiveClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix
 
 #Read the data
-df = pd.read_csv('data/news.csv')
+df = pd.read_csv("data/news.csv")
 
 #Shape and Head
 df.shape
